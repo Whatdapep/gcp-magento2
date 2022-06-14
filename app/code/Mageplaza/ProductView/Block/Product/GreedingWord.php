240f07d13a9b999374dd23703181972cca26249c
Whatdapep <pomming1997@gmail.com>
2022-06-14 10:31:29 +0700

    pom add real layout template block edit product view page

---
+++ app/code/Mageplaza/ProductView/Block/Product/GreedingWord.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+/**
+ * ProductView greeding word
+ */
+/**    POM Create Greeding Word Class */
+
+namespace Mageplaza\ProductView\Block\Product;
+
+
+
+class GreedingWord extends \Magento\Framework\View\Element\Template {
+
+}
---
+++ app/code/Mageplaza/ProductView/Block/StockLeft.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+/**
+ * ProductView greeding word
+ */
+/**    POM Create Greeding Word Class */
+
+namespace Mageplaza\ProductView\Block;
+
+
+
+class StockLeft extends \Magento\Framework\View\Element\Template {
+
+}
